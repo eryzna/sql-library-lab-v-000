ea3a91de86079f7c210d33acea7ae6dd33704dff
Elana Ryznar <eryzna@gmail.com>
2019-01-09 22:09:48 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -29,4 +29,10 @@ CREATE TABLE characters (
   motto TEXT,
   series_id INTEGER,
   author_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  book_id INTEGER,
+  chracter_id INTEGER
 )
